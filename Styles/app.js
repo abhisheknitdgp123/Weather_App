fe66dc050f5193e0308f73d39b88f05ab8c7dcd0
ABHISHEK KUMAR <abhisheknitdgp123@gmail.com>
2015-11-06 08:37:12 -0800

    Create app.js

---
+++ Styles/app.js
@@ -0,0 +1,37 @@
+body {
+    background-color: #ffffff;
+    color:#eeeeee;
+}
+
+.PageWrapper {
+    width: 100%;
+    margin: 0px auto;
+}
+
+.Heading {
+    text-align: center;
+}
+.App{
+text-align:center;
+}
+.TabContainer {
+    text-align: center;
+    margin-top: 30px;
+}
+
+.Tab {
+    margin: 0px 30px;
+    cursor: pointer;
+    font-size: 30px;
+    font-weight: bold;
+    color:#3399FF;
+    display:inline-block;
+}
+
+    .Tab:hover {
+        color: rgb(167,186,215);
+    }
+
+.ActiveButton {
+    color: #ffffff;
+}
